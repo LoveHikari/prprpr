66aa2c662af6ea24b9421d31cf43b2a7958ffcbf
汐榊少爷 <xmh.yxw@gmail.com>
2019-11-20 17:50:40 +0800

    添加了meta标签控制浏览器内核切换

--- statics/js/main.js
+++ statics/js/main.js
@@ -18,7 +18,7 @@ function getAchives(){
     t = ``;
     $.ajax({
         type:"GET",
-        url:"https://www.prprpr.ml/wp-json/wp/v2/posts?per_page=8&page=1",
+        url:"https://www.moedog.tk/wp-json/wp/v2/posts?per_page=8&page=1",
         dataType:"json",
         success:function(json){
             for(var i = 0;i < json.length;i++){
