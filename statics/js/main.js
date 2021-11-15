ac2c309b34bae6c7814750898ff70067dbe8b4f9
LoveHikari <xmh.yxw@gmail.com>
2021-11-15 17:27:24 +0800

    常规更新

--- statics/js/main.js
+++ statics/js/main.js
@@ -18,7 +18,7 @@ function getAchives(){
     t = ``;
     $.ajax({
         type:"GET",
-        url:"https://www.moedog.tk/wp-json/wp/v2/posts?per_page=8&page=1",
+        url:"https://www.moecat.tk/wp-json/wp/v2/posts?per_page=8&page=1",
         dataType:"json",
         success:function(json){
             for(var i = 0;i < json.length;i++){
@@ -34,8 +34,8 @@ function getAchives(){
 
 function gethitokoto(){
     $.ajax({
-        type:"POST",
-        url:"https://api.fczbl.vip/hitokoto/?encode=json",
+        type:"GET",
+        url:"https://api.moecat.tk/hitokoto?encode=json",
         dataType:"json",
         success:function(result){
             write(result.hitokoto);
@@ -56,7 +56,8 @@ function write(text){
 
 $(function(){
     $.ajax({
-        url:"https://api.fczbl.tk/163?type=playlist&id=2003373695",
+        type:"GET",
+        url:"https://api.moecat.tk/163?type=playlist&id=2003373695",
         success:function(e){
             var a = new APlayer({
                 element:document.getElementById("ap-f"),
@@ -77,20 +78,20 @@ $(function(){
 })
 
 // 对Date的扩展，将 Date 转化为指定格式的String
-// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
-// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-// 例子： 
-// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
-// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
-Date.prototype.Format = function(fmt){ //author: meizz 
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 例子：
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+Date.prototype.Format = function(fmt){ //author: meizz
     var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
     };
     if(/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1,(this.getFullYear() + "").substr(4 - RegExp.$1.length));
     for(var k in o)
